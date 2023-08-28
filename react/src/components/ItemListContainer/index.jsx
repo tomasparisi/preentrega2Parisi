c7ae349617f2baba923e329ab1eecd44d4b63af6
tomasparisi <129010856+tomasparisi@users.noreply.github.com>
2023-08-27 21:41:32 -0300

    nuevo

--- react/src/components/ItemListContainer/index.jsx
+++ react/src/components/ItemListContainer/index.jsx
@@ -1,19 +1,64 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./styles.css";
+import axios from "axios";
+import Item from "./Item/item";
 
-function ItemListContainer({ greetings }) {
-  const [saludo, setSaludo] = useState(greetings);
+const ItemListContainer = () => {
+  const [users, setUsers] = useState([]);
 
-  function cambio() {
-    setSaludo("6");
-  }
-
+  useEffect(() => {
+    axios("https://api.github.com/users").then((json) => setUsers(json.data));
+  }, []);
   return (
-    <div className="item-list-container">
-      <p>{saludo}</p>
-      <button onClick={cambio}>calcular stock</button>
+    <div>
+      {users.map((user) => (
+        <Item key={user.id} data={user} />
+      ))}
     </div>
   );
-}
+};
+
+// function ItemListContainer({ greetings }) {
+//   const [saludo, setSaludo] = useState(greetings);
+//   const [count, setCount] = useState(0);
+//   const [show, setShow] = useState(true);
+
+//   useEffect(() => {
+//     console.log(`la cuenta es: ${count}`);
+//   }, [count]);
+//   //useEffect se renderiza si tiene el arreglo de dependencias, en esta caso si no pongo [count], no habra en cambios en la consola
+//   // Las llamdas APIs tiene que estar dentro de los useEffect
+
+//   function hide() {
+//     setShow(false);
+//   }
+
+//   function aumenta() {
+//     setCount(count + 1);
+//   }
+
+//   function cambio() {
+//     setSaludo(7);
+//   }
+
+//   return (
+//     <div className="item-list-container">
+//       <p>{saludo}</p>
+//       <button onClick={cambio}>calcular stock</button>
+//       <button onClick={aumenta}>Aumentar + 1</button>
+//       <p>{count}</p>
+//       {show && <Im />}
+//       <button onClick={hide}>elimine img</button>
+//     </div>
+//   );
+// }
+
+// function Im() {
+//   return (
+//     <div>
+//       <p>AAAAAAAAA</p>
+//     </div>
+//   );
+// }
 
 export default ItemListContainer;
